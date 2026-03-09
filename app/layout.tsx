b42c1b34ca695e2d64ec587df7014d61b386f1f3
Arty <arty@Artys-Virtual-Machine.local>
2026-03-08 21:39:05 -0700

    add ga4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-1NJTY0HC0T"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+
+gtag('config', 'G-1NJTY0HC0T');`,
+          }}
+        />
+      </head>
       <body>
         <Layout>{children}</Layout>
       </body>
